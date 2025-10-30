ff81c9a378981d71373a69ad76cacd60792bf537
faridnovian <faridnovian61@gmail.com>
2025-10-30 10:44:27 +0700

    Merapikan UI pada Dashboard

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,7 +1,27 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import get_user_model
+
+from apps.users.models import Peran
+from apps.academics.models import Kelas, Mapel
+
+User = get_user_model()
 
 
-# Create your views here.
 class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = 'core/dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+
+        context['is_admin'] = user.is_superuser
+
+        # Hanya hitung dan tambahkan data ini jika pengguna adalah admin
+        if user.is_superuser:
+            context['total_akun'] = User.objects.count()
+            context['total_peran'] = Peran.objects.count()
+            context['total_kelas'] = Kelas.objects.count()
+            context['total_mapel'] = Mapel.objects.count()
+
+        return context
